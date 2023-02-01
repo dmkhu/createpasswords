1e12cb44f735085731e60ede7a4fee3d9c128df1
Dima Khutorov <96331923+dmkhu@users.noreply.github.com>
2023-02-01 15:38:20 +0300

    delet 1

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getRandomNum (min, max) {
 // создаем пароль
 function createPassword () {
     let newPassword = [];
-    let spisok = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9];
+    let spisok = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z",2,3,4,5,6,7,8,9];
     let i = 0;
     while (i < 10) {
         newPassword.push(spisok[getRandomNum(0, spisok.length - 1)]);
